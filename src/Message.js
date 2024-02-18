b881286b1da93200e9ed15788b137ea8758773df
justw <justwind14@yandex.ru>
2024-02-19 02:26:50 +0300

    finish

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,6 @@
 import './Message.css'
 
 function Message(props) {
-    const propsBool = true;
     return (
         <div>
             <p className={"Monkeyfact"} style={{color: (props.text.length < 100) ? 'red' : 'green'}}>{props.text} </p>
